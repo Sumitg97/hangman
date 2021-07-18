6ee53e83ce10492fcb76c0657dba18f4a2fb879e
Sumitg97 <sumitgarje1997@gmail.com>
2021-07-18 14:12:28 +0530

    controller added

--- src/main/java/com/game/hangman/application/HangmanApplication.java
+++ src/main/java/com/game/hangman/application/HangmanApplication.java
@@ -1,4 +1,4 @@
-package com.game.hangman;
+package com.game.hangman.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
