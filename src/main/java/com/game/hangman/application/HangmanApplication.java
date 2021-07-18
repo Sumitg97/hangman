29b808ddbef07c4c8bc3a80ac8263c7ace8dd20b
Sumitg97 <sumitgarje1997@gmail.com>
2021-07-18 15:39:15 +0530

    words read

--- src/main/java/com/game/hangman/application/HangmanApplication.java
+++ src/main/java/com/game/hangman/application/HangmanApplication.java
@@ -1,9 +1,11 @@
 package com.game.hangman.application;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class HangmanApplication {
 
 	public static void main(String[] args) {
